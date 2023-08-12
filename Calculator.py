4b66b68ceb822e72ebb6177f48424e272efdb494
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2023-08-12 15:34:50 -0400

    created Calculator using .py

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+a = 56;
+b = 5;
+
+print("The value o f ", a,"+", b, " is: ", a + b );
+print("The value o f ", a,"-", b, " is: ", a - b );
+print("The value o f ", a,"*", b, " is: ", a * b );
+print("The value o f ", a,"/", b, " is: ", a / b );
